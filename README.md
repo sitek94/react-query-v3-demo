0024354dd7203ef8e2a9f787c2608e9b958455c5
Maciek Sitkowski <maciej.sitkowski@netguru.com>
2022-07-23 11:49:22 +0200

    enable react-query devtools in production

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # React Query v3 Demo
 
-- [React Query v3 Docs](https://react-query-v3.tanstack.com/)
+- React Query
+  - [v3 Docs](https://react-query-v3.tanstack.com/)
+  - [Enable devtools in production](https://github.com/TanStack/query/discussions/3296)
+  - [Window focus refetching](https://react-query-v3.tanstack.com/guides/window-focus-refetching)
--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,14 @@
+import * as React from 'react'
 import {BrowserRouter as Router} from 'react-router-dom'
 import {QueryClient, QueryClientProvider} from 'react-query'
 import {ReactQueryDevtools} from 'react-query/devtools'
 import {Layout} from './layout'
+
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+  import('react-query/devtools/development').then(d => ({
+    default: d.ReactQueryDevtools,
+  })),
+)
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -12,11 +19,22 @@ const queryClient = new QueryClient({
 })
 
 export function App() {
+  const [showDevtools, setShowDevtools] = React.useState(true)
+
+  React.useEffect(() => {
+    window['toggleDevtools'] = () => setShowDevtools(old => !old)
+  }, [])
+
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
         <Layout />
         <ReactQueryDevtools initialIsOpen />
+        {showDevtools && (
+          <React.Suspense fallback={null}>
+            <ReactQueryDevtoolsProduction />
+          </React.Suspense>
+        )}
       </Router>
     </QueryClientProvider>
   )
