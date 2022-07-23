27c860842d2e5b29c41f3960d18a2d81ad593e31
Maciek Sitkowski <maciej.sitkowski@netguru.com>
2022-07-23 10:54:10 +0200

    use react 17 and react-query 3.33

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import {App} from './app'
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root'),
 )
