70bfb46f5039c0abc998bafb287df721a8a94bc5
Maciek Sitkowski <maciej.sitkowski@netguru.com>
2022-07-24 14:52:18 +0200

    add tailwind and daisyui

--- src/layout.tsx
+++ src/layout.tsx
@@ -6,15 +6,23 @@ import {api} from './api'
 export function Layout() {
   return (
     <>
-      <nav>
-        <h1>Star Wars</h1>
-        <NavLink exact to="/">
-          Home
-        </NavLink>
-        <NavLink to="/films">Films</NavLink>
-        <NavLink to="/people">People</NavLink>
+      <nav className="navbar bg-neutral text-neutral-content">
+        <h1 className="flex-1">Star Wars</h1>
+        <ul className="menu menu-horizontal">
+          <li>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/films">Films</NavLink>
+          </li>
+          <li>
+            <NavLink to="/people">People</NavLink>
+          </li>
+        </ul>
       </nav>
-      <main>
+      <main className="prose p-4">
         <Switch>
           <Route exact path="/films">
             <Films />
@@ -32,7 +40,22 @@ export function Layout() {
 }
 
 function Home() {
-  return <h1>Home</h1>
+  return (
+    <>
+      <h2>Home</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi
+        asperiores cupiditate debitis dolorem et ex ipsa iste magni maxime natus
+        nostrum odio optio, quas quibusdam sit tempore, velit! Illo?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam esse
+        est minus vel vitae! Eaque laudantium quidem rerum! Cupiditate eos
+        laudantium possimus vel? Architecto iusto quis sit. Expedita, maxime
+        quam?
+      </p>
+    </>
+  )
 }
 
 function People() {
